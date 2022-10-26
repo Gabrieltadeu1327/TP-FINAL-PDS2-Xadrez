9c415f975922fb8112505bde2cdd754af4b466fb
santastico <111901646+santastico@users.noreply.github.com>
2022-10-26 13:11:16 -0300

    Movimentação da torre implementada
    Finalmente corrigi os erros.

--- src/pieces/rook.cpp
+++ src/pieces/rook.cpp
@@ -1,4 +1,4 @@
-#include "C:\Users\gabri\OneDrive\Área de Trabalho\UFMG\PDS\TP-FINAL-PDS2-Xadrez-main\TP-FINAL-PDS2-Xadrez-main\include\pieces\rook.hpp"
+#include "include\pieces\rook.hpp"
 
 using namespace std;
 
@@ -9,9 +9,16 @@ rook::rook(string color, int *spot){
 // No caso da torre ela pode fazer movimentos apenas na horizontal e vertical, quantas casas quiser, sem pular nenhuma peça
 //lembre-se acessar a coordenada coluna: [x] --> [*spot], acessar a coordenada linha: [y] --> [*(spot+1)]
 void rook::def_possible_movements(){
+    //Pega a posição da peça
     int *spot = this->get_spot();
+
+    //caso a peça seja branca
     if (this->get_color() == "white"){
+
+        //contabilizadores de quantidade de movimentos possíveis para uma direção
         int go_leftW=0, go_rightW=0, go_topW=0, go_downW=0;
+
+        //contabilização de movimentos possíveis para a esquerda
         int x=*spot;
         for(int i=0; i<7; i++){
                 if(x == 0){
@@ -22,16 +29,20 @@ void rook::def_possible_movements(){
                     x--;
                 }
         }
+
+        //contabilização de movimentos possíveis para a direita
         x=*spot;
         for(int i=0; i<7; i++){
                 if(x == 7){
                     break;
                 }
                 else{
-                    go_rightW++
+                    go_rightW++;
                     x++;
                 }
         }
+
+        //contabilização de movimentos possíveis para cima
         int y=*(spot+1);
         for(int i=0; i<7; i++){
                 if(y==0){
@@ -42,7 +53,9 @@ void rook::def_possible_movements(){
                     y--;
                 }
         }
-        int y=*(spot+1);
+
+        //contabilização de movimentos possíveis para baixo
+        y=*(spot+1);
         for(int i=0; i<7; i++){
                 if(y==7){
                     break;
@@ -52,21 +65,29 @@ void rook::def_possible_movements(){
                     y++;
                 }
         }
+
+        //possíveis movimentos para direita
         x=*spot;
         for(int j=0; j<=go_rightW; j++){
             _possibles_movements[x][*(spot+1)]=1;
             x++;
         }
+
+        //possíveis movimentos para esquerda
         x=*spot;
         for(int j=0; j<=go_leftW; j++){
             _possibles_movements[x][*(spot+1)]=1;
             x--;
         }
+
+        //possíveis movimentos para cima
         y=*(spot+1);
         for(int j=0; j<=go_topW; j++){
             _possibles_movements[*spot][y]=1;
             j--;
         }
+
+        //possíveis movimentos para baixo
         y=*(spot+1);
         for(int j=0; j<=go_downW; j++){
             _possibles_movements[*spot][y]=1;
@@ -76,8 +97,13 @@ void rook::def_possible_movements(){
 
 // -------------------------------------------------------------------------------------------------------------------------------------------
 
+    //caso a peça seja preta
     if (this->get_color() == "black"){
+
+        //contabilizadores de quantidade de movimentos possíveis para uma direção
         int go_leftB=0, go_rightB=0, go_topB=0, go_downB=0;
+
+        //contabilização de movimentos possíveis para a esquerda
         int x=*spot;
         for(int i=0; i<7; i++){
                 if(x==0){
@@ -88,6 +114,8 @@ void rook::def_possible_movements(){
                     x--;
                 }
         }
+
+        //contabilização de movimentos possíveis para a direita
         x=*spot;
         for(int i=0; i<7; i++){
                 if(x==7){
@@ -98,6 +126,8 @@ void rook::def_possible_movements(){
                     x++;
                 }
         }
+
+        //contabilização de movimentos possíveis para cima
         int y=*(spot+1);
         for(int i=0; i<7; i++){
                 if(y==0){
@@ -108,6 +138,8 @@ void rook::def_possible_movements(){
                     y--;
                 }
         }
+
+        //contabilização de movimentos possíveis para baixo
         y=*(spot+1);
         for(int i=0; i<7; i++){
                 if(y==7){
@@ -118,25 +150,33 @@ void rook::def_possible_movements(){
                     y++;
                 }
         }
+
+        //possíveis movimentos para direita
         x=*spot;
         for(int j=0; j<=go_rightB; j++){
             _possibles_movements[x][*(spot+1)]=1;
             x++;
         }
+
+        //possíveis movimentos para esquerda
         x=*spot;
         for(int j=0; j<=go_leftB; j++){
             _possibles_movements[x][*(spot+1)]=1;
             x--;
         }
+
+        //possíveis movimentos para cima
         y=*(spot+1);
         for(int j=0; j<=go_topB; j++){
             _possibles_movements[*spot][y]=1;
-            j--;
+            y--;
         }
+
+        //possíveis movimentos para baixo
         y=*(spot+1);
         for(int j=0; j<=go_downB; j++){
             _possibles_movements[*spot][y]=1;
-            j++;
+            y++;
         }
     }
 }
