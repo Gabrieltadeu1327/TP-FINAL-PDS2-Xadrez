dcfb74fb368f82d7ef606dc3d58d7db7e9998509
CarlosMagnanimo <emagno123456789@gmail.com>
2022-10-26 12:05:57 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,14 @@
+#include "include/pieces/piece.hpp"
+#include "include/pieces/pawn.hpp"
 #include <iostream>
-#include<string.h>
-#include<map>
-#include<vector>
 
 using namespace std;
 
 int main(){
-	// string str = "1234567890ABCDE";
-	// string str2 = "0987654321ADCAE";
+	int spot[2] = {6, 0};
+	Pawn *p = new Pawn("white", spot);
+	p->def_possible_movements();
+	int *poss = p->get_possibles();
 
-	// map<char, int[2]> my;
-	// map<char, int[2]> my2;
-
-	// vector<map<char, int[2]>> vec;
-	// vec.push_back(my);
-	// vec.push_back(my2);
-
-	// for(auto it = vec.begin(); it != vec.end(); it++){
-	// 	for(int i = 0; i<10; i+=2){
-	// 		char c = 'A' + i/2;
-	// 		 = str[i];
-	// 		my[c][1] = str[i+1];
-			
-	// 	}
-	// }
-
-	// for(auto it = my.begin(); it!=my.end(); it++){
-	// 	cout<<it->first<< " "<<(char)(it->second)[0]<< " "<< (char)(it->second)[1]<<endl;
-	// }
-
-	std::cout<<"ola mundo";
+	
 }
