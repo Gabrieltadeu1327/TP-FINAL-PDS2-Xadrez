d3136b1667b0e2ea1e4be8716e0e665da1fcbe2a
Gabriel Cruzati <71570716+GabrielCruzati@users.noreply.github.com>
2022-11-26 16:29:42 -0300

    .

--- src/pieces/bishop.cpp
+++ src/pieces/bishop.cpp
@@ -13,9 +13,9 @@ Bishop::Bishop(string color){
 void Bishop::def_possible_movements(int* spot){
 
   // Rezeta a Matriz de movimentos possiveis
-    for (int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
-            assert( i < 0 || j > 8);
+    for (int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            assert( i < 0 || i >= 8 || j < 0 || j >= 8 );
             _possibles_movements[i][j]=0;
         }
     };
