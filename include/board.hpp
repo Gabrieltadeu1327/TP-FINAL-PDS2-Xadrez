647f99bc716b37c598a10a4a168b61bb900f12a7
santastico <111901646+santastico@users.noreply.github.com>
2022-11-29 22:06:02 -0300

    Doxygen do Board.hpp
    Acredito que terminei a documentação da board.

--- include/board.hpp
+++ include/board.hpp
@@ -45,17 +45,75 @@ class Board{
         Board();
         ~Board();
 
+        /**
+         * @brief Envia um poteiro correspondente ao ponto da peça
+         * @details O ponteiro gerado é utilizado em outras funções,
+         * como get_color.
+         * 
+         * @param spot ponto atual da peça, necessário para gerar o ponteiro.
+         * @return Piece* 
+         */
         Piece* get_piece(int* spot);
 
+        /**
+         * @brief Define movimentos válidos
+         * @details Além de definir os possíveis movimentos,
+         * a função define os movimentos válidos, excluindo da matriz
+         * possíveis movimentos inválidos.
+         * 
+         * @param spot ponto atual da peça, necessário para gerar o ponteiro. 
+         * @param mat matriz correspondente as posições das peças.
+         */
         void def_valid_moviments(int* spot, int mat[8][8]); 
 
+        /**
+         * @brief Muda a posição da peça e trata movimentos especiais
+         * @details Função que altera a posição da peça, tirando ela do seu 
+         * ponto inicial, levando para o ponto final. Além de cuidar das movimentações
+         * especiais, como promotion e roque.
+         * 
+         * @param atual_spot ponto atual da peça.
+         * @param spot ponto final da peça.
+         */
         void set_piece(int* atual_spot, int* spot);
 
+        /**
+         * @brief retorna o endereço da imagem da peça
+         * @details acessa o endereço de memória da imagem da peça
+         * retornando assim, o endereço para que consigamos ter as imagens na interface.
+         * 
+         * @param spot ponto da peça.
+         * @return std::string endereço de imagem da respectiva peça.
+         */
         std::string get_image(int* spot);
+
+        /**
+         * @brief Descobre a cor da peça
+         * @details Acessa a peça e retorna sua cor.
+         * 
+         * @param spot ponto da peça.
+         * @return std::string cor da peça.
+         */
         std::string get_collor(int* spot);
         
+        /**
+         * @brief Atualiza a matriz de ataque
+         * @details Recebendo a cor da peça, é possível determinar sua matriz 
+         * de ataque. 
+         * 
+         * @param cor cor da peça. 
+         */
         void refresh_atc_matriz(string cor);
 
+        /**
+         * @brief Determina se o rei está em cheque
+         * @details Podemos determinar se o rei está em posição de cheque
+         * ou não.
+         * 
+         * @param color cor da peça. 
+         * @return true sim, cheque.
+         * @return false não, sem cheque.
+         */
         bool ischeque(std::string color);
 
 
