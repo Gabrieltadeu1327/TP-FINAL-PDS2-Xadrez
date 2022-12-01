cb8439db8ab87528b53a9058503f4803a5ea5824
Ronaldo Gonçalves <100977386+Ronaldo-gsa@users.noreply.github.com>
2022-12-01 13:58:38 -0300

    atualização teste

--- tests/board_test.cpp
+++ tests/board_test.cpp
@@ -20,19 +20,17 @@ TEST_CASE("Testando Matrizes") {
     int spot[2];
     spot[0] = 6;//linha
     spot[1] = 2;//coluna
-    bool result =0;
 
-    int** mat_function = b->def_valid_moviments(spot);
+    int mat_function[8][8];
+    b->def_valid_moviments(spot,mat_function);
 
     for(int i= 0; i<8; i++){
         for(int j= 0; j<8; j++){
-            if(mat_function[i][j] != mat[i][j]){
-                result =1;
-                std::cout<< " Erro em: "<< i<< " "<< j<< " deveria ser: "<< mat[i][j] << " mas veio: "<< mat_function[i][j] <<std::endl;
-            }
+            CHECK_EQ(mat_function[i][j],mat[i][j]);
         }
     }
-    std::cout<< "Resultado: "<< result<< std::endl;
 
     //CHECK_THROWS(p != nullptr);
 }
+
+
